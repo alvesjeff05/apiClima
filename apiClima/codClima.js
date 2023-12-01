4c790aa1cc6cfb27c78d788de73b0f4740bbadf6
Jefferson Alves <100549763+alvesjeff05@users.noreply.github.com>
2023-12-01 16:28:25 -0300

    Add files via upload

---
+++ apiClima/codClima.js
@@ -0,0 +1,46 @@
+const key = "e0283766cee7e7e70be7e75409ae8042"
+
+function buscar(){
+    let cidade = document.querySelector(".input-cidade").value
+    dados(cidade)
+}
+
+async function dados(cidade){
+    let dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(Response=>Response.json());
+    inserirDados(dados);
+    temperatura(dados);
+    umidade(dados);
+    previsao(dados);
+    imagem(dados);
+}
+
+function inserirDados(dados){
+    console.log(dados)
+    document.querySelector(".titulo-cidade").innerHTML = "Tempo em " + dados.name
+}
+
+function temperatura(dados){
+    console.log(dados)
+    document.querySelector(".graus").innerHTML = "A temperatura é de " + dados.main.temp + "ºC"
+}
+
+function umidade(dados){
+    console.log(dados)
+    document.querySelector(".umidade").innerHTML = "Umidade é de " + dados.main.humidity + "%"
+}
+
+function previsao(dados){
+    console.log(dados)
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+}
+
+function imagem(dados){
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+}
+
+document.addEventListener("keypress", function (e){
+    if (e.key === "Enter") {
+        const btn = document.querySelector(".btn");
+        btn.click();
+    }
+})
